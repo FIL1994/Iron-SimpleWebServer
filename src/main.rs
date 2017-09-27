e9b8b00e0abab1b3a24ea4d5afb0dc2ce7e864cf
FIL1994 <phil.vanraalte@gmail.com>
2017-09-27 10:07:44 -0400

    better comments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+/*
+ * @author Philip Van Raalte
+ * @date 2017-09-26
+ *
+ * A simple web server made with Iron framework
+ */
 extern crate iron;
 extern crate router;
 extern crate mount;
@@ -12,7 +18,9 @@ use staticfile::Static;
 
 use std::path::Path;
 
-///Main function
+/// Main function
+/// Setup the router and a mount (to serve static files).
+/// Then start the server on port 3000
 fn main() {
     let mut router = Router::new();
     router
@@ -28,11 +36,15 @@ fn main() {
     Iron::new(mount).http("localhost:3000").unwrap();
 }
 
+/// SayHello function
+/// Respond with a message of "This request was routed!"
 fn say_hello(req: &mut Request) -> IronResult<Response> {
     println!("Running send_hello handler, URL path: {}", req.url.path().join("/"));
     Ok(Response::with((status::Ok, "This request was routed!")))
 }
 
+/// Handler function
+/// handle a request
 fn handler(req:&mut Request) -> IronResult<Response> {
     let ref query = req.extensions.get::<Router>().unwrap()
         .find("query").unwrap_or("/");
